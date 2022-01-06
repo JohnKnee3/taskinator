8f60604717bae7234ddb3e87ec89e6b11e8f5813
john.a.clark3 <john.a.clark3@gmail.com>
2022-01-06 16:42:20 -0700

    4.2.9
    if (!taskNameInput || !taskTypeInput) {
        alert("You need to fill out the task form!");
        return false;
      }
    if statement that checks if the user selected nothing and yells at them if they haven't  in an alert.  Then it returns a false for reasons.
    
    formEl.reset();
    This resets the text in the text box after use to it's default state.  Only works on the form element.

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -6,6 +6,14 @@ var taskFormHandler = function(event) {
   var taskNameInput = document.querySelector("input[name='task-name']").value;
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+  //check if input values
+  if (!taskNameInput || !taskTypeInput) {
+    alert("You need to fill out the task form!");
+    return false;
+  }
+  
+  formEl.reset();
+
   // package up data as an object
   var taskDataObj = {
       name: taskNameInput,
@@ -32,3 +40,4 @@ var createTaskEl = function (taskDataObj) {
 };
 
 formEl.addEventListener("submit", taskFormHandler);//supposed to work on enter
+
