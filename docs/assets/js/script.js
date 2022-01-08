add0f57b4fbf01f0d39d3018a3bbb616dd54810a
john.a.clark3 <john.a.clark3@gmail.com>
2022-01-08 15:14:11 -0700

    4.4.5
    saveTask();
    called the newly created function in the funcs createTasksEl, completeEditTask, taskStatusChangeHandler and deleteTask and then
    
    var saveTasks = function() {
    localStorage.setItem("tasks", JSON.stringify(tasks));
    created a saveTask func and added JSON.stringify to make it show the OBJECT (which is all it was saying before) and show it as a string that looks something like
    [{"name":"Dog","type":"Web","status":"to do","id":0}] in the dev tools

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -60,7 +60,8 @@ var createTaskEl = function(taskDataObj) {
   taskDataObj.id = taskIdCounter;
 
   tasks.push(taskDataObj);
-  
+
+  saveTasks();  
 
   // increase task counter for next unique id
   taskIdCounter++;
@@ -121,6 +122,8 @@ var completeEditTask = function(taskName, taskType, taskId) {
     }
   };
 
+  saveTasks();
+
   alert("Task Updated!");
 
   // remove data attribute from form
@@ -170,6 +173,7 @@ var taskStatusChangeHandler = function(event) {
     }
   }
   console.log(tasks);
+  saveTasks();
 };
 
 var editTask = function(taskId) {
@@ -214,7 +218,13 @@ var deleteTask = function(taskId) {
 
   //reassign tasks array to be the same as updatedTaskArr
   tasks = updatedTaskArr;
+  saveTasks();
 };
+
+var saveTasks = function() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+
+}
 
 // Create a new task
 formEl.addEventListener("submit", taskFormHandler);
