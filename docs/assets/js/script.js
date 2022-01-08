f401cc72f8981ca8fbd56f213be9747d3bb0c38f
john.a.clark3 <john.a.clark3@gmail.com>
2022-01-08 11:32:32 -0700

    4.3.10
    var tasksInProgressEl = document.querySelector("#tasks-in-progress");
    var tasksCompletedEl = document.querySelector("#tasks-completed");
    added the ul's from the HTML to the JS
    
    pageContentEl.addEventListener("change", taskStatusChangeHandler);
    calls the new function by listening for a "change" which listens for when a <form> value changes
    
    var taskStatusChangeHandler = function(event) {
    start of new function passing in event so we can target things
    
      //get the task items id
      var taskId = event.target.getAttribute("data-task-id");
    an example of looking at what we are targeting and then getting it's attribute which is the number we've assigned to data-task-id and bringing it into the func and then assigning it to the var taskId.
    
      //get the currently selected option's value and convert to lowercase
      var statusValue = event.target.value.toLowerCase();
    gets the value of what is targeted in this case the name to do, tasks in progress or completed and sets it to lowercase to future proof it and assigns it to the var statusValue.
    
      //find the parent task item element based on the id
      var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
    finds the li using querySelector and then gets specific with what list by taking the newly created var taskId and then assigns this entire specific list to the var taskSelected.
    
      if (statusValue === "to do") {
        tasksToDoEl.appendChild(taskSelected);
      }
    this checks if the value of statusValue is "to do".  If so then it appends the entire li from it's current ul and moves it to the var tasksToDoEl  which references the id of the ul in HTML..
    
      else if (statusValue === "in progress") {
        tasksInProgressEl.appendChild(taskSelected);
      }
    this checks if the value of statusValue is "in progress".  If so then it appends the entire li from it's current ul and moves it to the var  tasksInProgressEl which references the id of the ul in HTML..
    
      else if (statusValue === "completed") {
        tasksCompletedEl.appendChild(taskSelected);
      }
    this checks if the value of statusValue is "completed".  If so then it appends the entire li from it's current ul and moves it to the var tasksCompletedEl  which references the id of the ul in HTML.
    
    };
    closes the func taskStatusChangeHandler
    
    Big thing to note is when appending the li it does not create a copy.  JS needs document.createElement to do something like that.  It instead just finds the li we targeted and appends that existing element somewhere else.

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,7 +1,10 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var tasksInProgressEl = document.querySelector("#tasks-in-progress");
+var tasksCompletedEl = document.querySelector("#tasks-completed");
 var taskIdCounter = 0;
 var pageContentEl = document.querySelector("#page-content");
+
 
 var taskFormHandler = function(event) {
   event.preventDefault();
@@ -137,6 +140,27 @@ var taskButtonHandler = function(event) {
   }
 };
 
+var taskStatusChangeHandler = function(event) {
+  //get the task items id
+  var taskId = event.target.getAttribute("data-task-id");
+
+  //get the currently selected option's value and convert to lowercase
+  var statusValue = event.target.value.toLowerCase();
+
+  //find the parent task item element based on the id
+  var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+  if (statusValue === "to do") {
+    tasksToDoEl.appendChild(taskSelected);
+  }
+  else if (statusValue === "in progress") {
+    tasksInProgressEl.appendChild(taskSelected);
+  }
+  else if (statusValue === "completed") {
+    tasksCompletedEl.appendChild(taskSelected);
+  }
+};
+
 //edit func
 var editTask = function(taskId) {
   //get task list item element
@@ -159,3 +183,5 @@ var deleteTask = function(taskId) {
 };
 
 pageContentEl.addEventListener("click", taskButtonHandler);
+
+pageContentEl.addEventListener("change", taskStatusChangeHandler);
